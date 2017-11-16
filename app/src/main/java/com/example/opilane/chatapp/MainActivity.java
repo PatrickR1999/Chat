ba18134eb69e2b3d91c4e0c5c2f63dc633fa53f7
Opilane TTHK <opilane@tthk.ee>
2017-11-16 11:43:19 +0200

    Initial commit

---
+++ app/src/main/java/com/example/opilane/chatapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.opilane.chatapp;
+
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.util.Date;
+
+
+public class ChatMessage {
+        private String messageText;
+        private String messageUser;
+        private long messageTime;
+
+public ChatMessage(String messageText, String messageUser){
+        this.messageText = messageText;
+        this.messageUser = messageUser;
+
+        }
+
+
+        }
+
+
+
+
